74b35766ea705d1a6d7b24de577b72950deb4359
Adityakirtane <2022.aditya.kirtane@ves.ac.in>
2025-02-19 18:04:18 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-324PB6NVQ4"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-324PB6NVQ4');
-</script>
